6fcaceed77ba8d9288097f2aa143f5bda55ee126
Yevhen <96053362+YevhenLisitskyiDev@users.noreply.github.com>
2022-06-23 21:29:00 +0300

    Delete src directory

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  REACT_APP_SUPABASE_URL: "https://wjtvjpdkngnplqzxhmgr.supabase.co",
-  REACT_APP_SUPABASE_PUBLIC_KEY:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqdHZqcGRrbmducGxxenhobWdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTU0Njc5NDksImV4cCI6MTk3MTA0Mzk0OX0.iXql2QS79lMncW99Q65Fu0a8gnZh2bHUDh4CLoe1vZ4",
-  ORGANIZATION_ID: 1
-};
-
-export default config
--- src/store/demo.ts
+++
@@ -1,5 +0,0 @@
-const demo = {
-    test: ""
-}
-
-export default demo
