bf50220c322254fa93da4cbb12a5a19b641b77b3
Yevhen <96053362+YevhenLisitskyiDev@users.noreply.github.com>
2022-06-28 09:47:09 +0300

    Container component added to courses list

--- src/widgets/courses/CoursesList.tsx
+++ src/widgets/courses/CoursesList.tsx
@@ -2,15 +2,21 @@ import React, { useMemo, useState, useEffect } from "react";
 import BlogPostCard from "./../../teleporthq/components/blog-post-card";
 import getCourses from "./../../services/courses/getCourses";
 import store from "./../../store/index";
+import Container from "../../components/layout/Container";
 
 const CoursesList = () => {
   useMemo(() => getCourses(), []);
 
   const courses = store.courses.hook();
+  const error = store.error.hook();
+
   let content: any = "";
   if (courses !== null)
-    content = store.courses.value.map((course: any) => <BlogPostCard {...course} />);
-  return <div>{content}</div>;
+    content = store.courses.value.map((course: any) => (
+      <BlogPostCard {...course} />
+    ));
+  if (error) content = error?.message;
+  return <Container>{content}</Container>;
 };
 
 export default CoursesList;
