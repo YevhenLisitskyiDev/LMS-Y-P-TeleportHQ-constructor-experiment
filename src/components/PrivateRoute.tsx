82474a7e546229b8d670d5f8f6640186221984ea
YevhenLisitskyiDev <yevhen.lisitskyi.dev@gmail.com>
2022-07-12 20:53:12 +0300

    Private route update

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -2,32 +2,15 @@ import React, { FC } from "react";
 import { Route, Redirect } from "react-router-dom";
 import store from "./../store";
 
-interface PrivateRouteProps {
-  path: string;
-  component: JSX.Element;
-  exact?: boolean;
-}
-
-// create functional private route react component
-
-const PrivateRoute = ({
-  path,
-  component: Component,
-  exact,
-}: PrivateRouteProps) => {
+// crete a new component that will render either the route or a redirect using renderProps
+const PrivateRoute: FC<any> = ({ component: Component, ...rest }) => {
   const session = store.auth.session.hook();
-
   return (
     <Route
-      path={path}
-      exact={exact}
-      render={(props) => {
-        if (session) {
-          return <Component {...props} />;
-        } else {
-          return <Redirect to="/login" />;
-        }
-      }}
+      {...rest}
+      render={(renderProps) =>
+        session ? <Component {...renderProps} /> : <Redirect to="/login" />
+      }
     />
   );
 };
