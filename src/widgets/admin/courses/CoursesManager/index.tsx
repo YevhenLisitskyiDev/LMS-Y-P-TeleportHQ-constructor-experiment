ad9d24a06e09768b69d5d775ad9983ef56b1b4fd
YevhenLisitskyiDev <yevhen.lisitskyi.dev@gmail.com>
2022-07-13 14:21:55 +0300

    Created course manager draft component

---
+++ src/widgets/admin/courses/CoursesManager/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import config from "../../../../config";
+import supabase from "../../../../services/supabase";
+
+const CoursesManager = () => {
+  const [courses, setCourses] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(async () => {
+    let { data: courses, error } = await supabase
+      .from("courses")
+      .select("*")
+      .eq("organization_id", config.ORGANIZATION_ID);
+
+    if (!error) setCourses(courses);
+    if (error) setError(error);
+  }, []);
+
+  if (courses.length && courses.length === 0) return <div>No courses</div>;
+  if (error) return <div>{error?.message}</div>;
+  if (!courses) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>Courses</h1>
+      <ul>
+	{courses.map((course) => (
+	  <li key={course.id}>
+	    {course.name} - {course.description}
+	  </li>
+	))}
+      </ul>
+    </div>
+};
