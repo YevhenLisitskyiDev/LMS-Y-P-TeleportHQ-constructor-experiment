301f47a97ee64bc93c9e4cc8c1655f67e6ae5928
Yevhen <96053362+YevhenLisitskyiDev@users.noreply.github.com>
2022-07-13 21:19:13 +0300

    admin lessons list edited

--- src/components/admin/LessonsList.tsx
+++ src/components/admin/LessonsList.tsx
@@ -1,39 +1,37 @@
-improt React, {useState,useEffect} from "react";
+import React, { useState, useEffect, memo } from "react";
 import styled from "styled-components";
-import supabase from "../../../services/supabase";
+import supabase from "./../../services/supabase";
 import store from "./../../store/";
 
-const LessonsList = ({data, isSelected}) => {
+const LessonsList = memo(({ data, isSelected }) => {
   const [lessons, setLessons] = useState(null);
   const error = store.error.hook();
   const id = data.id;
 
   useEffect(async () => {
-	console.log("lessonsListt mounted");
-    if(isSelected) {
-	let { data: lessons, error } = await supabase
-      .from("lessons")
-      .select("*")
-      .eq("course_id", id);
+    console.log("lessonsListt mounted");
+    if (isSelected) {
+      let { data: lessons, error } = await supabase
+        .from("lessons")
+        .select("*")
+        .eq("course_id", id);
 
-    if (!error) store.lessons.next(lessons);
-    if (error) store.error.next(error);
+      if (!error) setLessons(lessons);
+      if (error) store.error.next(error);
     }
   }, []);
 
   if (error) return <div>{error?.message}</div>;
   if (!lessons) return <div>Loading lessons...</div>;
-  if (lessons.length && lessons.length === 0) return <div>No lessons</div>;
+  if (lessons.length === 0) return <div>No lessons</div>;
 
   return (
     <div>
       {lessons.map((lesson, index) => (
-	<div>
-		{lesson.name}
-	</div>
-	      ))}
+        <div>{lesson.name}</div>
+      ))}
     </div>
   );
-}
+});
 
 export default LessonsList;
