090528f9f8b703758a9a584f95695b6657d20dc7
YevhenLisitskyiDev <yevhen.lisitskyi.dev@gmail.com>
2022-07-14 14:33:39 +0300

    Refactored courses lisn in CoursesManagerComponent

--- src/components/admin/CoursesManager/CoursesManagerItems.tsx
+++ src/components/admin/CoursesManager/CoursesManagerItems.tsx
@@ -2,10 +2,9 @@ import React, { useState, useCallback, useRef, useEffect } from "react";
 import styled from "styled-components";
 import { deleteCourseHandler } from "./UDHandlers.ts";
 import { XButton } from "../../../components/buttons";
-import { PlusMinusIcon } from "../../Accordion/AccordionComponents";
-import Curtain from "../../Curtain";
 import LessonsList from "../LessonsList";
-import OpenModalButton from "./../../OpenModalButton.tsx"
+import OpenModalButton from "./../../OpenModalButton.tsx";
+import ToggleListItem from "../../ToggleListItem";
 
 const CousreTitleLayoutWrapper = styled.div`
   width: 95%;
@@ -17,79 +16,43 @@ const CousreTitleLayoutWrapper = styled.div`
 
   & button {
     margin-left: 20px;
-z-index: 1;
+    z-index: 1;
     position: relative;
     top: unset;
   }
 `;
 
-export const CousreTitleLayout = ({ course }) => {
-  console.log(course);
-
+export const CousreTitleLayout = ({ data: course }) => {
   return (
     <CousreTitleLayoutWrapper>
       {course.name} - {course.description}
       <XButton
         onClick={(e) => {
           deleteCourseHandler(course.id);
-        }}
-      >
+        }}>
         Delete
       </XButton>
     </CousreTitleLayoutWrapper>
   );
 };
 
-const CourseTitleWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-`;
-
-// export const CousreContentLayout = ({ data: course }) => {
-//   return (
-//     <CousreTitleLayoutWrapper>
-//       {course.name} - {course.description}
-//     </CousreTitleLayoutWrapper>
-//   );
-// };
-
-const CourseItemWrapper = styled.div`
-  width: 100%;
-  position: relative;
-  margin-bottom: 20px;
-  & * {
-    z-index: 1;
-  }
-`;
+export const CousreContentLayout = ({ data: course, isSelected }) => {
+  return (
+    <>
+      <LessonsList id={course.id} isSelected={isSelected} />
+      <OpenModalButton modalContent={() => <span>{course.id}</span>}>
+        Add lesson
+      </OpenModalButton>
+    </>
+  );
+};
 
 export const CourseItem = ({ data: course }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [loadItemsTrigger, setLoadItemsTrigger] = useState(false);
-
-  // Make toggle is open function with useCallback
-  const toggleIsOpen = (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      setIsOpen(false);
-      return;
-    }
-    if (!loadItemsTrigger) setLoadItemsTrigger(true);
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <CourseItemWrapper>
-      <CourseTitleWrapper onClick={(e) => toggleIsOpen(e)}>
-        <PlusMinusIcon isOpen={isOpen} />
-        <CousreTitleLayout course={course} />
-      </CourseTitleWrapper>
-
-      <Curtain isOpen={isOpen}>
-        <LessonsList id={course.id} isSelected={loadItemsTrigger} />
-        <OpenModalButton modalContent={()=> <span>{course.id}</span>}>Add lesson</OpenModalButton>
-      </Curtain>
-    </CourseItemWrapper>
+    <ToggleListItem
+      data={course}
+      itemTitleLayout={CousreTitleLayout}
+      itemContentLayout={CousreContentLayout}
+    />
   );
 };
