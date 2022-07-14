779e526fd543b193879559867b333fff52d8a774
Yevhen <96053362+YevhenLisitskyiDev@users.noreply.github.com>
2022-07-14 20:28:59 +0300

    Removed unused code from OpenModalButton component

--- src/components/OpenModalButton.tsx
+++ src/components/OpenModalButton.tsx
@@ -4,31 +4,12 @@ import { XButton, ColoredButton } from "./buttons.tsx";
 import store from "../store";
 
 const OpenModalButton = ({ children, modalContent, mode }) => {
-  useEffect(() => {
-    const escFunction = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        store.modal.isOpen.next(false);
-      }
-    };
-    document.addEventListener("keydown", escFunction, false);
-
-    return () => document.removeEventListener("keydown", escFunction, false);
-  }, []);
-
+ 
   //  create toggle modal window function with use callback hook
   const toggleModalWindow = React.useCallback(() => {
     store.modal.content.next(modalContent);
     store.modal.isOpen.toggle();
   }, [modalContent]);
-
-  const clickOutsideModalWindow = React.useCallback(
-    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      if (event.target === event.currentTarget) {
-        toggleModalWindow();
-      }
-    },
-    [toggleModalWindow]
-  );
 
   return (
     <ColoredButton mode={mode} onClick={toggleModalWindow}>
