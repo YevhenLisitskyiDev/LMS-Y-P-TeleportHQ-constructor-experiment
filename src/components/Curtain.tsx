925ffa0d0d097a602c98c6cd9a48820a454ba85a
YevhenLisitskyiDev <yevhen.lisitskyi.dev@gmail.com>
2022-07-14 11:23:56 +0300

    Created Curtain component

--- src/components/Curtain.tsx
+++ src/components/Curtain.tsx
@@ -1,1 +1,15 @@
-{}
+import React from "react";
+import styled from "styled-components";
+
+// Make Curtain component that will hide content behind it using height 0
+const Curtain = styled.div<{ isOpen: boolean }>`
+  height: ${({ isOpen }) => (isOpen ? "0" : "100%")};
+  transition: height 0.3s ease-in-out;
+  overflow: hidden;
+
+  width: 100%;
+  background: #fff;
+  z-index: 1;
+`;
+
+export default Curtain;
